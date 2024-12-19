5af2156156eb2f0ed56a5246cf2f7e04f68ceaee
Nicholas2k4 <nicholas.s2k4@gmail.com>
2024-12-19 21:13:13 +0700

    chore: cleanup routes

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,13 @@ Route::middleware(['check:user,admin'])->group(function () {
     Route::get('/teams', [TeamController::class, 'show'])->name('teams');
 });
 
-
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
-
-Route::get('/signup', function () {
-    return view('auth.signup');
-})->name('signup');
-
+Route::get('/signup', [AuthController::class, 'showSignup'])->name('signup');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/submit-signup', [AuthController::class, 'signup_authenticate'])->name('signup.post');
-
 Route::post('/signin-success', [AuthController::class, 'login_authenticate'])->name('signin.post');
-
+Route::get('/loginGoogle', [AuthController::class, 'redirectToGoogle'])->name('user.auth');
+Route::get('/auth/callback', [AuthController::class, 'loginGoogle'])->name('login.process');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/teams/create', [TeamController::class, 'create'])->name('teams.create');
@@ -40,6 +32,4 @@ Route::middleware('auth')->group(function () {
     Route::post('/addMember', [TeamController::class, 'addMember'])->name('addMember');
 });
 
-Route::get('/loginGoogle', [AuthController::class, 'redirectToGoogle'])->name('user.auth');
-
-Route::get('/auth/callback', [AuthController::class, 'loginGoogle'])->name('login.process');
+Route::middleware(['check:admin'])->group(function () {});
